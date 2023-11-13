96c8450efdbcc5a9c39e22518dac5198532569cc
stephx202 <stephhernandez202@gmail.com>
2023-11-13 15:46:58 -0800

    Feature 2 and 3

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-// const userName = prompt("What is your name?");
-let guess = prompt("Guess a number any number!");
-function play(guess) {
-    let secretNumber = 7;
-    if(guess===null){
+let userInput //prompt("Guess a number")
+
+function newGuess(){
+  userInput = prompt("Guess a number")
+  count++
+ }  
+ let count = 0
+
+//const userName = prompt("What is your name?");
+function play(){
+  let secretNumber = 7;
+  newGuess()
+  //let guess = prompt("Guess a number any number!");
+  while(true){
+    //let guess = prompt("Guess a number any number!");
+    if(userInput==null){
       alert("Have a nice day!");
-    }else if(guess < secretNumber) {
-     alert("Too Low! Try again!")
-     return guess= prompt("Guess a number any number!")
-    }else if (guess > secretNumber) {
-      alert("Too high!")
-      return guess = prompt("Guess a number any number!")
-    }else if(guess==secretNumber){
-      alert("Correct!");
+      break;
+    }
+    if(userInput < secretNumber) {
+     alert("Too Low! Try again!");
+     newGuess()
+    }if (userInput > secretNumber) {
+      alert("Too high! Try again");
+      newGuess()
+    if(userInput==secretNumber){
+      if(count === 1){
+        alert('Congrats you guessed correctly on your first try!');
+      }
+      alert(`Congrats you guessed correctly in ${count} tries`);
+      break;
+    }else if(newGuess() != number){
+      break;
     }
   }
-  
-  play(guess);
+}console.log(count)
+}
+
+play()
