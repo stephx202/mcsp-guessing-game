9aabc598b94d06a68ce723bf605320eeaafba60b
stephx202 <stephhernandez202@gmail.com>
2023-11-14 10:29:54 -0800

    feature 1-7

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-function lowScore(){
-if(count<scores['userName']){
-  scores['userName'] = count
+function lowScore(name){
+if(scores[name] == undefined){
+    scores[name] = count
+    alert(`NEW LOW SCORE: ${count} `)
+}else if(scores[name]>count){ 
+  scores[userName] = count
   alert(`NEW LOW SCORE: ${count} `)
 }
 }
@@ -12,31 +15,30 @@ const userName = prompt("Enter your username")
 
 function newGuess(){
   userInput = prompt("Guess a number")
-  count++
- }  
+  keepCount()
+  }  
 
 function playAgain(){
-  let rerun = prompt(`Do you want to play again ${userName}. yes or no`)
+  let rerun = prompt(`Do you want to play again ${userName}? yes or no`)
   if(rerun === "yes"){
     play()
   } 
 }
-
 function keepCount(){
   allGuess.push(userInput)
+  count++
 }
 
 let count = 0
- let allGuess = []
+let allGuess = []
 
-//const userName = prompt("What is your name?");
 function play(){
+  debugger;
   let secretNumber = 7;
+  count = 0
   newGuess()
-  //let guess = prompt("Guess a number any number!");
   while(true){
-    lowScore()
-    //let guess = prompt("Guess a number any number!");
+    
     if(userInput==null){
       alert("Have a nice day!");
       break;
@@ -44,21 +46,21 @@ function play(){
     if(userInput < secretNumber) {
      alert(`Sorry ${userName} too Low! Try again!`);
      newGuess()
-     keepCount()
     }if (userInput > secretNumber) {
       alert(`Sorry ${userName} too high! Try again!`);
       newGuess()
-      keepCount()
     }
     if(userInput==secretNumber){
       if(count === 1){
         alert(`Congrats ${userName} you guessed correctly on your first try!`);
+       lowScore(userName);
         playAgain();
       }
       alert(`Congrats ${userName} you guessed correctly in ${count} tries. Your guesses were `+allGuess);
+      lowScore(userName);
       playAgain();
       break;
-    }else if(newGuess() == NaN){
+    }else if(userInput == NaN){
       break;
     }
   }
